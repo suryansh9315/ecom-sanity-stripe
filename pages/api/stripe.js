3db013b2bb3099a4f8fb7fdc7255b78b9e5c2e3b
suryansh9315 <suryansh.sharma9315@gmail.com>
2022-05-29 10:12:40 +0530

    Resolved some errors

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
                     {shipping_rate:'shr_1L42HELHdmbQBmKfQs4RS7JX'},
                     {shipping_rate:'shr_1L42GSLHdmbQBmKfMTzkrVqM'}
                 ],
-                success_url: `${req.headers.origin}/Success`,
+                success_url: `${req.headers.origin}/success`,
                 cancel_url: `${req.headers.origin}/canceled`,
             });
             res.status(200).json(session);
